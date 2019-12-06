7ecf667dda9f22e377c4fd1a73c1de6f1f71f47d
Thomasin Durgin <td@thomasin.com>
2019-12-06 03:48:58 +0000

    lesson 10

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,6 @@
 class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def new
     @gram = Gram.new
   end
@@ -7,13 +9,13 @@ class GramsController < ApplicationController
   end
 
   def create
-  @gram = Gram.create(gram_params)
+  @gram = current_user.grams.create(gram_params)
   if @gram.valid?
     redirect_to root_path
   else
     render :new, status: :unprocessable_entity
   end
-  end
+end
 
   private
 
